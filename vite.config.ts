495c285169cdb342416b4f1781cece3b0092b15b
komal <chaudharykomal1225@gmail.com>
2025-11-29 17:30:19 +0545

    Allow Render host in Vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -11,6 +11,7 @@ export default defineConfig({
   },
   preview: {
     host: "0.0.0.0",
-    port: 5173
+    port: 5173,
+    allowedHosts: ['project-ujmf.onrender.com'] 
   }
 })
